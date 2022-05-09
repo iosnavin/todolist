1c97c2d2c3b96cfea4ebb74bb3ccbf3c7da55418
Naveen <ios.nreddy@gmail.com>
2022-05-09 15:49:40 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Naveen Reddy on 09/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
